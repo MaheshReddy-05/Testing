8c08d29070839aa03a6fafe8fed8e8091d0fa4be
Mahesh Reddy <nagamaheshreddy3@gmail.com>
2023-04-25 12:40:39 +0530

    Added Custom rfc

---
+++ src/components/Buttons/BasicButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ExternalLinkButton() {
+  return (
+    <div>
+        <a href='/blog'>Click Me</a>
+        
+    </div>
+  )
+}
+
---
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import ExternalLinkButton from '@site/src/components/Buttons/BasicButton.jsx';
+
+export default {
+  ...ComponentTypes,
+  'custom-myAwesomeNavbarItem': ExternalLinkButton,
+};
